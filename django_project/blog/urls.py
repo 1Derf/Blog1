b51e150880f87c1be55bacaa1e60314f294cf7c0
derf <quaderone@pm.me>
2024-11-24 07:35:43 -0700

    Bug fixes

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,19 +1,28 @@
-#from tempfile import template
-
-#from django.contrib import admin
-#from django.contrib.auth import views as auth_views
 from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView
+)
+
 from . import views
 #from .views import views as auth_views
-from django.contrib.auth.views import LogoutView, LoginView
-
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>', UserPostListView.as_view(), name='user-posts'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
     path('login/', views.LoginView, name='login'),
-    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
 
 
